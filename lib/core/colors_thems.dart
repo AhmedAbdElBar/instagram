7e010050a01703adc5c6842be6437a79b3246e46
BANDOLA112 <ahmedabdelbar231@gmail.com>
2025-01-10 03:14:53 +0200

    coloring thems

--- lib/core/colors_thems.dart
+++ lib/core/colors_thems.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
 class ThemingColor {
-  Color mainColor = Colors.red;
+  Color bottonColor = Color.fromRGBO(0, 163, 255, 1);
+  Color darkGrayColor = Color.fromRGBO(38, 38, 38, 1);
+  Color lightGrayColor = Color.fromRGBO(223, 223, 223, 1);
+  Color blueFontColor = Color.fromRGBO(56, 151, 240, 1);
 }

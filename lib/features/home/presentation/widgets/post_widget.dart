d597429f5dc2451d2623088c1e4b7e9986c040a0
Ahmed abd elbar <145259898+BANDOLA112@users.noreply.github.com>
2025-01-27 00:07:38 +0200

    solve the problem

--- lib/features/home/presentation/widgets/post_widget.dart
+++ lib/features/home/presentation/widgets/post_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/core/circleAvatar_with_border.dart';
 import 'package:instagram/core/colors_thems.dart';
-<<<<<<< HEAD
 import 'package:instagram/features/home/presentation/widgets/actions_for_posts.dart';
 
-class Post extends StatefulWidget {
+class Post extends StatelessWidget {
   final String email;
   final String profileImage;
   final String postImage;
@@ -16,63 +15,15 @@ class Post extends StatefulWidget {
   });
 
   @override
-  _PostState createState() => _PostState();
-}
-
-class _PostState extends State<Post> with SingleTickerProviderStateMixin {
-  bool isLoved = false;
-  late AnimationController _animationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  void toggleLove() {
-    setState(() {
-      isLoved = !isLoved;
-      if (isLoved) {
-        _animationController.forward();
-      } else {
-        _animationController.reverse();
-      }
-    });
-  }
-
-=======
-
-class Post extends StatelessWidget {
-  late String email;
-  late String profileImage;
-  late String postImage;
-  Post(
-      {required this.email,
-      required this.profileImage,
-      required this.postImage});
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
-  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-<<<<<<< HEAD
         border: Border.all(color: ThemingColor.lightGrayColor, width: 1),
       ),
-=======
-          border: Border.all(color: ThemingColor.lightGrayColor, width: 1)),
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Header Section
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -81,40 +32,29 @@ class Post extends StatelessWidget {
                 child: Row(
                   children: [
                     InkWell(
-<<<<<<< HEAD
                       child: CircleavatarWithBorder(
-                          chick: 10, size: 20, image: widget.profileImage),
+                        chick: 10,
+                        size: 20,
+                        image: profileImage,
+                      ),
                     ),
                     SizedBox(width: 10),
-=======
-                        child: CircleavatarWithBorder(
-                            chick: 10, size: 20, image: profileImage)),
-                    SizedBox(
-                      width: 10,
-                    ),
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-<<<<<<< HEAD
-                          widget.email,
-=======
                           email,
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
                           style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 13),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
                         ),
                         Text(
                           "Sponsored",
                           style: TextStyle(fontSize: 12),
-                        )
+                        ),
                       ],
-<<<<<<< HEAD
                     ),
-=======
-                    )
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
                   ],
                 ),
               ),
@@ -124,95 +64,39 @@ class Post extends StatelessWidget {
                   AssetImage("assets/icons/menu_dots.png"),
                   size: 40,
                 ),
-              )
+              ),
             ],
           ),
+          // Post Image
           Container(
-<<<<<<< HEAD
             width: double.infinity,
             child: Image(
-              image: AssetImage(widget.postImage),
+              image: AssetImage(postImage),
               fit: BoxFit.cover,
             ),
           ),
+          // Actions (Like, Comment, Bookmark)
           ActionsForPosts(),
-=======
-              width: double.infinity,
-              child: Image(
-                image: AssetImage(postImage),
-                fit: BoxFit.cover,
-              )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 110,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    InkWell(
-                      child: Icon(
-                        Icons.favorite_border_outlined,
-                        size: 30,
-                      ),
-                    ),
-                    InkWell(
-                      child: ImageIcon(
-                        AssetImage("assets/icons/comment.png"),
-                        size: 30,
-                      ),
-                    ),
-                    InkWell(
-                      child: Icon(
-                        Icons.send_outlined,
-                        size: 30,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.bookmark_outline,
-                  size: 30,
-                ),
-              )
-            ],
-          ),
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
+          // Likes, Comments, and Description
           Padding(
-            padding: const EdgeInsets.only(left: 8, right: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 8),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-<<<<<<< HEAD
                   "100 likes",
-=======
-                  "100 like",
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 Text.rich(
                   TextSpan(
                     children: [
                       TextSpan(
-<<<<<<< HEAD
-                        text: "${widget.email} ",
+                        text: "$email ",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       TextSpan(
                         text: 'Lorem ipsum dolor sit amet, consectetur...',
                       ),
-=======
-                          text: "$email",
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text: 'Lorem ipsum dolor sit amet, consectetur...'),
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
                     ],
                   ),
                 ),
@@ -221,14 +105,10 @@ class Post extends StatelessWidget {
                   style: TextStyle(
                     color: ThemingColor.darkGrayColor,
                   ),
-<<<<<<< HEAD
                 ),
-=======
-                )
->>>>>>> fc7b9a2048f45adce46d48df3bb4210310619cf7
               ],
             ),
-          )
+          ),
         ],
       ),
     );

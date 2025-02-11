f5262eef6772ff60f93c19df92fa2c7bf8872201
Ahmed abd elbar <145259898+BANDOLA112@users.noreply.github.com>
2025-02-11 20:24:12 +0200

    modifications for language_provider

--- lib/core/provider/language_provider.dart
+++ lib/core/provider/language_provider.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageProvider extends ChangeNotifier {
-  String _localeLanguage = "en"; 
+  String _localeLanguage = "en"; // Default language
 
-  String get localeLanguage => _localeLanguage; 
+  String get localeLanguage => _localeLanguage;
 
-  void changeLanguage(String lang) {
+  LanguageProvider() {
+    _loadLanguage(); // Load the saved language when the provider is initialized
+  }
+
+  void changeLanguage(String lang) async {
     if (_localeLanguage != lang) {
       _localeLanguage = lang;
-      notifyListeners(); 
+      notifyListeners();
+      await _saveLanguage(lang); // Save the language to SharedPreferences
     }
+  }
+
+  Future<void> _saveLanguage(String lang) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('selected_language', lang);
+  }
+
+  Future<void> _loadLanguage() async {
+    final prefs = await SharedPreferences.getInstance();
+    _localeLanguage = prefs.getString('selected_language') ?? "en";
+    notifyListeners(); // Notify listeners after loading the language
   }
 }
